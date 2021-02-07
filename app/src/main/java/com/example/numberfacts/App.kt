a57b047488ea8a3e8d1961cccb619d7a7ab4b8ab
Ekaterinin <ekaterinin@Ekaterinin.local>
2021-02-07 19:37:26 +0300

    fixed navigation between fragments

--- app/src/main/java/com/example/numberfacts/App.kt
+++ app/src/main/java/com/example/numberfacts/App.kt
@@ -1,6 +1,9 @@
 package com.example.numberfacts
 
 import android.app.Application
+import ru.terrakok.cicerone.Cicerone
+import ru.terrakok.cicerone.NavigatorHolder
+import ru.terrakok.cicerone.Router
 
 class App: Application() {
 
