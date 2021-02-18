dec3853a65bf57071f4daa9e1cc5974c179a70c2
Ekaterinin <ekaterinin@Ekaterinin.local>
2021-02-18 23:54:28 +0300

    fix remarks

--- app/src/main/java/com/example/numberfacts/ServiceBuilder.kt
+++ app/src/main/java/com/example/numberfacts/ServiceBuilder.kt
@@ -16,7 +16,6 @@ object ServiceBuilder {
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .client(client)
         .build()
 
